50a05b804f5fb5fc6f602226e02f984755a94e8e
Divyanshoo Sinha <107339622+DivyanshooSinha@users.noreply.github.com>
2025-08-25 23:28:44 +0530

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using EmployeeApi.Models;
+
+namespace EmployeeApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")] // Swagger shows JSON
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public EmployeesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET: api/Employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployees()
+        {
+            var employees = new List<Employee>();
+
+            using (SqlConnection conn = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            using (SqlCommand cmd = new SqlCommand("getEmp", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                await conn.OpenAsync();
+
+                using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        employees.Add(new Employee
+                        {
+                            EmpNo = reader.GetInt32(reader.GetOrdinal("EmpNo")),
+                            Employee_Name = reader.GetString(reader.GetOrdinal("Employee_Name")),
+                            //Salary = reader.GetInt32(reader.GetOrdinal("Salary")),
+                            Department_ID = reader.GetInt32(reader.GetOrdinal("Department_ID"))
+                        });
+                    }
+                }
+            }
+
+            return Ok(employees);
+        }
+    }
+
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Employee_Name { get; set; }
+        public int Department_ID { get; set; }
+    }
+}
